747d62ae4ab0e8b9f25547ea2a590dbf3ce935e1
libhinny <libhinny@gmail.com>
2024-01-30 15:44:58 -0300

    update

--- dataset.py
+++ dataset.py
@@ -11,8 +11,7 @@ print(tf._version_)
 
 
 # caminho do dataset
-dados = "C:\Users\libhi\Downloads\dataset_SignLanguage.zip"
-treino = tf.keras.utils.get_file("dataset_SignLanguage.zip")
+
 
 # lista de arquivos
 
