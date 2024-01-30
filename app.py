d25495f7ff1df5ba12ed7a84ad7442c59e8158f8
[Ytalo.dev ] <[ytalodias206@gmail.com ]>
2024-01-30 15:46:11 -0300

    Create file app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import streamlit as st
+import numpy as np 
+
+st.title("Brazilian Sign Language Sign Recognition")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+from tensorflow import keras
+
 
 x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
