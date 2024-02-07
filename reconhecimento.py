4de2a66096c9f18449ac7881b543cbe71308af1c
franciscalorraynes <franciscalorrayne5@gmail.com>
2024-02-07 13:56:32 -0300

    uptdade - correção

--- reconhecimento.py
+++ reconhecimento.py
@@ -1,38 +1,65 @@
 import cv2
 import mediapipe as mp
 
+webcam = cv2.VideoCapture()
+ip = "https://10.194.40.94:8080/video"
+webcam.open(ip)  # para detectar a webcam
 
-webcam = cv2.VideoCapture(0)
-hand = mp.solutions.hands.Hands(max_num_hands=2)
-mpDraw = mp.solutions.drawing_utils
+hand = mp.solutions.hands.Hands(max_num_hands=2)  # mapear as mãos
+mpDraw = mp.solutions.drawing_utils  # desenhar as ligações entre os pontos das mãos
 
-def Ligar_webcam():
-    
-    try:
-        while True:
-            check, image = webcam.read()
-            if not check:
-                print("Erro ao capturar a imagem da webcam.")
-                break
+try:
+    while True:
+        check, image = webcam.read()
+        if not check:
+            print("Erro ao capturar a imagem da webcam.")
+            break
+            # passar o parametro de imagem
+        imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hand.process(imgRGB)
 
-            imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = hand.process(imgRGB)
+        altura, largura, _ = image.shape
 
-            altura, largura, _ = image.shape
+        handsPoint = results.multi_hand_landmarks
+        if handsPoint:
+            for points in handsPoint:
+                mpDraw.draw_landmarks(
+                    image, points, mp.solutions.hands.HAND_CONNECTIONS
+                )
 
-            handsPoint = results.multi_hand_landmarks
-            if handsPoint:
-                for points in handsPoint:
-                    mpDraw.draw_landmarks(image, points, mp.solutions.hands.HAND_CONNECTIONS)
+                for id, cord in enumerate(points.landmark):
+                    cx, cy = int(cord.x * largura), int(cord.y * altura)
+                    cv2.putText(
+                        image,
+                        str(id),
+                        (cx, cy + 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 0, 0),
+                        2,
+                    )
 
-                    for id, cord in enumerate(points.landmark):
-                        cx, cy = int(cord.x * largura), int(cord.y * altura)
-                        cv2.putText(image, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        cv2.imshow("Imagem webcam", image)
+        key = cv2.waitKey(1)
+        if key == 27:  # ESC
+            break
+finally:
+    webcam.release()
+    cv2.destroyAllWindows()
 
-            cv2.imshow("Imagem da sua webcam", image)
-            key = cv2.waitKey(5)
-            if key == 27:  # ESC
-                break
-    finally:
-        webcam.release()
-        cv2.destroyAllWindows()
+
+# import cv2
+
+
+# if webcam.isOpened():
+#     validacao, frame = webcam.read()
+#     while validacao:
+#         validacao, frame = webcam.read()
+#         cv2.imshow("Video da Webcam", frame)
+#         key = cv2.waitKey(1)
+#         if key == 27:  # ESC
+#             break
+#     cv2.imwrite("FotoLira.png", frame)
+
+# webcam.release()
+# cv2.destroyAllWindows()
