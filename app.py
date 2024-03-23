bebcda6daf932824d0ea2c08f5c51d82aed06105
Ytalo Developer <[ytalodias206@gmail.com ]>
2024-03-23 16:30:44 -0300

    falta só a camera, testem

--- app.py
+++ app.py
@@ -4,104 +4,77 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 import mediapipe as mp
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-import time
 import h5py
 
-# Carregar o modelo treinado
-
-# Abra o arquivo diretamente com uma codificação específica
 with h5py.File('model.h5', 'r', driver='core') as f:
-    # Carregue o modelo
     model = load_model(f, compile=False)
 
-label_to_text =  {'bus': 0 , 'bank': 1 , 'car' : 2 , 'formation': 3 , 'hospital' : 4 ,'I' : 5 , 'man' : 6 , 'motorcycle' : 7 , 'my' : 8 , 'supermarket' : 9 , 'we' : 10 ,
-             'woman'  : 11 , 'you' : 12 , 'you (plural)' : 13  , 'your' : 14 }
 
-# Função para processar o frame de vídeo e fazer a previsão do objeto
-def predict_object(frame):
-    # Redimensionar o frame para o tamanho esperado pelo modelo
-    resized_frame = cv2.resize(frame, (48, 48))
+label_to_text = {0: 'bus', 1: 'bank', 2: 'car', 3: 'formation', 4: 'hospital', 5: 'I', 6: 'man', 7: 'motorcycle', 8: 'my', 9: 'supermarket', 10: 'we', 11: 'woman', 12: 'you', 13: 'you (plural)', 14: 'your'}
+
+
+def predict_object(hand_roi):
     
-    # Converter o frame para escala de cinza
-    gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+    resized_hand = cv2.resize(hand_roi, (120, 213))
     
-    # Normalizar os pixels da imagem
-    normalized_frame = gray_frame.astype('float32') / 255.0
     
-    # Pré-processar o frame para o modelo
-    preprocessed_frame = normalized_frame.reshape(1, 48, 48, 1)
+    normalized_hand = resized_hand.astype('float32') / 255.0
     
-    # Fazer a previsão
-    predicted_class = final_model_mlp.predict(preprocessed_frame).argmax()
+    
+    predicted_class = final_model.predict(np.expand_dims(normalized_hand, axis=0)).argmax()
     predicted_object = label_to_text[predicted_class]
     
     return predicted_object
 
-# Função para reconhecer o objeto em uma imagem
-def recognize_object(image):
-    # Fazer a previsão do objeto
-    predicted_object = predict_object(image)
-    return predicted_object
 
-# Função para redimensionar a imagem
-def resize_image(image, target_size):
-    # Redimensionar a imagem
-    resized_image = cv2.resize(image, target_size)
-    
-    return resized_image
-
-# Definir a classe do transformador de vídeo
 class VideoTransformer(VideoTransformerBase):
-    def _init_(self):
-        # Inicializar o modelo de detecção de mãos
-        self.hand_detection = mp.solutions.hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    def __init__(self):
+        pass
         
     def transform(self, frame):
-        # Converter o frame para um array numpy
+        
         frame_bgr = np.array(frame.to_image())
 
-        # Converter de BGR para RGB
+        
         frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
         
-        # Detectar mãos no frame
-        results = self.hand_detection.process(frame_rgb)
+     
+        results = hands.process(frame_rgb)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                # Extrair coordenadas da mão
+                
                 hand_points = np.array([[lmk.x, lmk.y] for lmk in hand_landmarks.landmark]).astype(np.float32)
                 xmin, ymin = np.min(hand_points, axis=0)
                 xmax, ymax = np.max(hand_points, axis=0)
                 xmin, ymin, xmax, ymax = int(xmin * frame_rgb.shape[1]), int(ymin * frame_rgb.shape[0]), \
                                          int(xmax * frame_rgb.shape[1]), int(ymax * frame_rgb.shape[0])
                 
-                # Recortar a região da mão
+                
                 hand_roi = frame_rgb[ymin:ymax, xmin:xmax]
                 
-                # Reconhecer o objeto (mão)
-                predicted_object = recognize_object(hand_roi)
+                predicted_object = predict_object(hand_roi)
                 
-                # Desenhar a mão detectada no frame
+                
                 frame_rgb = cv2.putText(frame_rgb, predicted_object, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
                 print(predicted_object)
         
         return frame_rgb
 
-#exibir imagem
 st.sidebar.image("https://www.mjvinnovation.com/wp-content/uploads/2021/07/mjv_blogpost_redes_neurais_ilustracao_cerebro-01-1024x1020.png")
-# Definir o título da página
+
 st.sidebar.title('Reconhecimento de :red[Sinais] :ok_hand:')
 
-# Breve descrição do projeto
+
 st.sidebar.info("""
 ## Reconhecimento de Mãos - Projeto
 
 Este projeto visa desenvolver um programa capaz de utilizar uma rede neural treinada para detectar mãos em tempo real por meio da câmera do usuário. Usando um modelo de rede neural CNN. 
 """)
 
-# Iniciar o streamlit webrtc
+
 webrtc_streamer(key="hand-recognition-1", video_processor_factory=VideoTransformer)
 
-# Integrantes do projeto
+
 st.sidebar.write("""
 ## Integrantes
 
--- reconhecimento.py
+++ reconhecimento.py
@@ -1,65 +1,111 @@
 import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
 import mediapipe as mp
+import h5py
+import tensorflow.compat.v1 as tf
+from mediapipe.python.solutions import hands
 
-webcam = cv2.VideoCapture()
-ip = 0 # aqui é o ip do celular
-webcam.open(ip)  # para detectar a webcam
+# Corrigir aviso sobre a função depreciada
+tf.disable_v2_behavior()
 
-hand = mp.solutions.hands.Hands(max_num_hands=2)  # mapear as mãos
-mpDraw = mp.solutions.drawing_utils  # desenhar as ligações entre os pontos das mãos
-
+# Corrigir erro FileNotFoundError
 try:
-    while True:
-        check, image = webcam.read()
-        if not check:
-            print("Erro ao capturar a imagem da webcam.")
-            break
-            # passar o parametro de imagem
-        imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hand.process(imgRGB)
+    hand_detection = hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
+except FileNotFoundError as e:
+    print("Erro ao inicializar o modelo de detecção de mãos:", e)
 
-        altura, largura, _ = image.shape
+# Carregar o modelo treinado
+with h5py.File('model.h5', 'r', driver='core') as f:
+    # Carregue o modelo
+    final_model = load_model(f, compile=False)
 
-        handsPoint = results.multi_hand_landmarks
-        if handsPoint:
-            for points in handsPoint:
-                mpDraw.draw_landmarks(
-                    image, points, mp.solutions.hands.HAND_CONNECTIONS
-                )
+label_to_text = {
+    0: 'bus',
+    1: 'bank',
+    2: 'car',
+    3: 'formation',
+    4: 'hospital',
+    5: 'I',
+    6: 'man',
+    7: 'motorcycle',
+    8: 'my',
+    9: 'supermarket',
+    10: 'we',
+    11: 'woman',
+    12: 'you',
+    13: 'your',
+    14: 'bus',
+    15: 'you (plural)'
+}
 
-                for id, cord in enumerate(points.landmark):
-                    cx, cy = int(cord.x * largura), int(cord.y * altura)
-                    cv2.putText(
-                        image,
-                        str(id),
-                        (cx, cy + 10),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5,
-                        (255, 0, 0),
-                        2,
-                    )
+def predict_object(frame):
+    # Converter o frame para uma imagem colorida (3 canais de cor)
+    frame_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    resized_frame = cv2.resize(frame_color, (120, 213))
+    
+    # Normalizar os pixels
+    normalized_frame = resized_frame.astype('float32') / 255.0
+    
 
-        cv2.imshow("Imagem webcam", image)
-        key = cv2.waitKey(1)
-        if key == 27:  # ESC
-            break
-finally:
-    webcam.release()
-    cv2.destroyAllWindows()
+    preprocessed_frame = normalized_frame.transpose((1, 0, 2))  # Inverter as dimensões (120, 213) para (213, 120)
+    preprocessed_frame = preprocessed_frame.reshape(1, 213, 120, 3)
+    
+    # Fazer a previsão
+    predicted_class = final_model.predict(preprocessed_frame).argmax()
+    predicted_object = label_to_text[predicted_class]
+    
+    return predicted_object
+
+# Função para reconhecer o objeto em uma imagem
+def recognize_object(image):
+    # Fazer a previsão do objeto
+    predicted_object = predict_object(image)
+    return predicted_object
 
 
-# import cv2
+hand_detection = mp.solutions.hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
+# Capturar vídeo da câmera
+cap = cv2.VideoCapture(0)
 
-# if webcam.isOpened():
-#     validacao, frame = webcam.read()
-#     while validacao:
-#         validacao, frame = webcam.read()
-#         cv2.imshow("Video da Webcam", frame)
-#         key = cv2.waitKey(1)
-#         if key == 27:  # ESC
-#             break
-#     cv2.imwrite("FotoLira.png", frame)
+while True:
+    # Ler o próximo frame
+    ret, frame = cap.read()
+    
+    # Converter o frame para RGB
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Detectar mãos no frame
+    results = hand_detection.process(frame_rgb)
+    
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Extrair coordenadas da mão
+            hand_points = np.array([[lmk.x, lmk.y] for lmk in hand_landmarks.landmark]).astype(np.float32)
+            xmin, ymin = np.min(hand_points, axis=0)
+            xmax, ymax = np.max(hand_points, axis=0)
+            xmin, ymin, xmax, ymax = int(xmin * frame_rgb.shape[1]), int(ymin * frame_rgb.shape[0]), \
+                                     int(xmax * frame_rgb.shape[1]), int(ymax * frame_rgb.shape[0])
 
-# webcam.release()
-# cv2.destroyAllWindows()
+            # Recortar a região da mão
+            hand_roi = frame_rgb[ymin:ymax, xmin:xmax]
+
+            # Reconhecer a mao
+            predicted_object = recognize_object(hand_roi)
+
+            # Desenhar a mão detectada no frame
+            frame = cv2.putText(frame, predicted_object, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+            frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+
+    # Exibir o frame
+    cv2.imshow('Hand Detection', frame)
+    
+    # q sai do loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar os recursos
+cap.release()
+cv2.destroyAllWindows()
